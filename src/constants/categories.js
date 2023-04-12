3781559afd9462705fe01d9d6c649209585193a1
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-04-12 16:00:18 +0200

    Initial commit

---
+++ src/constants/categories.js
@@ -0,0 +1,12 @@
+const categories = [
+  { id: 0, name: "all" },
+  { id: 1, name: "education and training" },
+  { id: 2, name: "adult education, vocational, staff, and personnel training" },
+  { id: 3, name: "e-learning and computer" },
+  { id: 4, name: "schools and special education" },
+  { id: 5, name: "health, first aid, and medical" },
+  { id: 6, name: "higher and university level" },
+  { id: 7, name: "other" }
+];
+
+export default categories;
---
+++ src/pages/[category].jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const category = () => {
+  return (
+    <div>[category]</div>
+  )
+}
+
+export default category
