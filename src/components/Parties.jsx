89fcaafb511fc5b9928f652af20797b5e3ec580a
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-04-19 05:56:48 +0200

    Modify styles

--- src/components/Parties.jsx
+++ src/components/Parties.jsx
@@ -1,70 +1,62 @@
 import { v4 as uuidv4 } from "uuid";
+import styles from "@/styles/Parties.module.css";
 
 const Parties = ({ parties }) => (
-    <div className="tender-card-parties">
-      {parties.map((party) => {
-        let email = "";
-        let contactName = "";
-        let url = "";
-        let buyerProfile = "";
+  <div className="tender-card-parties">
+    {parties.map((party) => {
+      let email = "";
+      let contactName = "";
+      let url = "";
+      let buyerProfile = "";
 
-        if (party.contactPoint) {
-          if (party.contactPoint.email) {
-            email = party.contactPoint.email;
-          }
-          if (party.contactPoint.name) {
-            contactName = party.contactPoint.name;
-          }
-          if (party.contactPoint.url) {
-            url = party.contactPoint.url;
-          }
-          if (party.details) {
-            if (party.details.buyerProfile) {
-              buyerProfile = party.details.buyerProfile;
-            }
+      if (party.contactPoint) {
+        if (party.contactPoint.email) {
+          email = party.contactPoint.email;
+        }
+        if (party.contactPoint.name) {
+          contactName = party.contactPoint.name;
+        }
+        if (party.contactPoint.url) {
+          url = party.contactPoint.url;
+        }
+        if (party.details) {
+          if (party.details.buyerProfile) {
+            buyerProfile = party.details.buyerProfile;
           }
         }
-        return (
-          <div className="tender-card-party" key={uuidv4()}>
-            <p className="tender-card-party-name" key={uuidv4()}>
-              <span>Party name:</span> {party.name}
-            </p>
-            <p className="tender-card-party-contact-name" key={uuidv4()}>
-              <span>Contact name:</span> {contactName}
-            </p>
-            <div className="tender-card-party-contact-email" key={uuidv4()}>
-              <span>Contact email:</span>{" "}
-              <p key={uuidv4()}>{email}</p>
-            </div>
-            <div className="tender-card-party-links">
-              <p
-                className="tender-card-party-contact-url"
-                key={uuidv4()}
-              >
-                <span>Contact website:</span> {url}
-              </p>
-              <p
-                className="tender-card-party-buyer-profile"
-                key={uuidv4()}
-              >
-                <span>Buyer website:</span> {buyerProfile}
-              </p>
-            </div>
-
-            <div className="tender-card-party-roles">
-              <p className="tender-card-party-roles-label">Roles:</p>
-              {party.roles.map((role) => (
-                  <p className="tender-card-party-role" key={uuidv4()}>
-                    {role}
-                  </p>
-                ))}
-            </div>
-            <hr />-
+      }
+      return (
+        <div key={uuidv4()}>
+          <p key={uuidv4()}>
+            <strong>Party name:</strong> {party.name}
+          </p>
+          <p key={uuidv4()}>
+            <strong>Contact name:</strong> {contactName}
+          </p>
+          <div key={uuidv4()}>
+            <strong>Contact email:</strong> <p key={uuidv4()}>{email}</p>
           </div>
-          
-        );
-      })}
-    </div>
-  );
+          <div>
+            <p key={uuidv4()}>
+              <strong>Contact website:</strong> {url}
+            </p>
+            <p key={uuidv4()}>
+              <strong>Buyer website:</strong> {buyerProfile}
+            </p>
+          </div>
+          <div>
+            <strong>Roles:</strong>
+            <div className={styles.roles}>
+            {party.roles.map((role) => (
+              <p key={uuidv4()} className={styles.role}>{role}</p>
+            ))}
+            </div>
+          </div>
+          <hr />
+        </div>
+      );
+    })}
+  </div>
+);
 
 export default Parties;
